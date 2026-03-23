2530d88bde3c92f188e00eedd5c30fe655fcaf45
Hayden Frank <haydog2006@gmail.com>
2026-03-23 13:59:22 -0400

    fix: removed TODO file and refined constants TODO comment

--- backend/app/core/security/constants.py
+++ backend/app/core/security/constants.py
@@ -1,4 +1,4 @@
-# TODO: genearte secret key : DONE : should this be an env variable?
+# TODO: genearte secret key and store it in env variable for prod
 SECRET_KEY = "b0a593270cdd78965ba8614f317a22d7bd65be15f9b7082d5808f79b927eaf8"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
