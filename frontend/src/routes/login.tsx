31e8fd240f962fdf9acde4283fbf2ebd152f254d
Elijah De Guzman <63167131+Cloud-Multiplayer@users.noreply.github.com>
2026-03-30 23:37:18 -0400

    feat: auth css transition

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,15 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
-import gatorBg from '/GainesvilleDownTown.jpg'
 import LoginForm from '../components/LoginForm.tsx'
+import '../styles/auth.css'
 
 export const Route = createFileRoute('/login')({ component: App })
 
 function App() {
   return (
-    <div
-      className="bg-cover bg-center min-h-screen flex items-center justify-center p-4 text-white"
-      style={{ backgroundImage: `url(${gatorBg})` }}
-    >
+    <div className="backgroundImage">
       <LoginForm />
     </div>
   )
