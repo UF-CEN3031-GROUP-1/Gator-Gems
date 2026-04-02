2d1029514793fe0cf45e9aa92e1c64efe762d5e2
jeremiah-hidayat-uf <jeremiahhidayat@ufl.edu>
2026-04-02 11:34:57 -0400

    feat: changed profile card to dedicated profile page

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,13 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import ProfileCard from '../components/ProfileCard'
-import '../styles/auth.css'
 
 export const Route = createFileRoute('/profile')({ component: App })
 
 function App() {
-  return (
-    <div className="backgroundImage">
-      <ProfileCard />
-    </div>
-  )
+  return <ProfileCard />
 }
