be7fab0bc1190601e5d449ea2e60ed5f402b414f
Elijah De Guzman <63167131+Cloud-Multiplayer@users.noreply.github.com>
2026-04-14 18:07:04 -0400

    precommit

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -9,13 +9,25 @@ function App() {
       <div className="be">
         <h1 className="title">Welcome</h1>
         <p className="text">
-          Gator Gems is a state of the art application that takes advantage of user submitted hidden gems all around
-          Gainesville as to provide the best places for old and new Gainsville citizens to gain a new experience in the city.
+          Gator Gems is a state of the art application that takes advantage of
+          user submitted hidden gems all around Gainesville as to provide the
+          best places for old and new Gainsville citizens to gain a new
+          experience in the city.
         </p>
 
         <div className="actions">
-          <button onClick={() => window.location.href = "/gemMap"} className="explore-button">Explore the Map</button>
-          <button onClick={() => window.location.href = "/signup"} className="gt-button">Get Started</button>
+          <button
+            onClick={() => (window.location.href = '/gemMap')}
+            className="explore-button"
+          >
+            Explore the Map
+          </button>
+          <button
+            onClick={() => (window.location.href = '/signup')}
+            className="gt-button"
+          >
+            Get Started
+          </button>
         </div>
 
         <div className="row">
@@ -34,7 +46,5 @@ function App() {
         </div>
       </div>
     </div>
-     
   )
 }
-
